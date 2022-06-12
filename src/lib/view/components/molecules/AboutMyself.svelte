29f843da32f7577f0d5dbc60f7d602d723c3fe09
nishioyuta-0812 <yutanishi0812@gmail.com>
2022-06-12 11:51:12 +0900

    fix

--- src/lib/view/components/molecules/AboutMyself.svelte
+++ src/lib/view/components/molecules/AboutMyself.svelte
@@ -3,7 +3,7 @@
 
 </script>
 
-<div class="about-mayself w-full sm:w-[40%] sm:pt-[100px] text-xl sm:text-3xl text-center sm:text-start">
+<div class="about-mayself w-full sm:w-[40%] pt-6 sm:pt-[100px] text-xl sm:text-3xl text-center sm:text-start">
   <p>生年月日: 平成9年8月12日</p>
   <p>出身地: 埼玉県</p>
   <p>趣味: アニメ鑑賞、ドライブ、筋トレなど</p>
@@ -12,8 +12,6 @@
 
 <style lang="scss">
   .about-mayself {
-    
-    padding-top: 100px;
     p {
       margin-bottom: 20px;
     }

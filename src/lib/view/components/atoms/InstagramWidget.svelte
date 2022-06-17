803e2e410c53160fa532f98c80023dbf5b189883
nishioyuta-0812 <yutanishi0812@gmail.com>
2022-06-17 20:36:34 +0900

    インスタのウィジェットの高さの調整

--- src/lib/view/components/atoms/InstagramWidget.svelte
+++ src/lib/view/components/atoms/InstagramWidget.svelte
@@ -5,8 +5,8 @@
 <!-- svelte-ignore a11y-missing-attribute -->
 <iframe 
     src="https://cdn.lightwidget.com/widgets/050eaff8d8ef518594e3352f4976267c.html" 
-    scrolling="no" 
+    scrolling="auto" 
     allowtransparency={true}
     class="lightwidget-widget" 
-    style="width:100%;border:0;overflow:hidden;height:100%;overflow:scroll;overflow-x:hidden;">
+    style="width:100%;border:0;overflow:hidden;height:90%;overflow:scroll;overflow-x:hidden;">
   </iframe>
